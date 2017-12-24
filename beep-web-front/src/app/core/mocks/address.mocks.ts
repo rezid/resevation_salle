98ca126df417baa20cae4a3e98a7a0630e7a6cc6
zidane <zidane.rezzak@gmail.com>
2017-12-24 00:13:06 -0800

    add mocks data

---
+++ beep-web-front/src/app/core/mocks/address.mocks.ts
@@ -0,0 +1,29 @@
+import { Address } from '../models/address/address';
+
+
+
+const address_list: Address[] = [
+    {
+        streetNumber: 12,
+        streetName: 'Rue marchall king',
+        city: 'Noisy-Le-Grand',
+        postalCode: 93320,
+        country: 'France',
+    },
+    {
+        streetNumber: 123,
+        streetName: 'Rue Presidant Roosvelt king',
+        city: 'Paris',
+        postalCode: 75003,
+        country: 'France',
+    },
+    {
+        streetNumber: 4444,
+        streetName: 'Rue le mere doré',
+        city: 'Paris',
+        postalCode: 75007,
+        country: 'France',
+    },
+];
+
+export const ADDRESS_LIST = address_list;
---
+++ beep-web-front/src/app/core/mocks/room.mocks.ts
@@ -0,0 +1,104 @@
+import { Room } from '../models/room/room';
+import { ADDRESS_LIST } from './address.mocks';
+
+const roomList: Room[] = [
+    {
+        _id: 'sdsdg54654sdg',
+        nameName: 'Le Void',
+        description: 'Dansgsdg, Dans un aire plein de musie oriontal sdgsgs sdgsdg, Dans un aire plein de musie oriontal sdgsgs sdgsdg, ',
+        address: ADDRESS_LIST[0],
+        type: 'Salle de musique',
+        price: 203, // par jour
+        size: 150, // en metre carré
+        capacity: 200,
+        roomCount: 2, // nombre de chambre
+        creationDate: new Date(),
+        uid: 'sv54sd6g4sd6g' // utilisateur id
+    },
+
+    {
+        _id: 'gq45d64564',
+        nameName: 'La Vesselle',
+        description: 's<lknfglk sgdsdgmusie oriontal sdgsgs sdgsdg, Dans un aire plein de musie oriontal sdgsgs sdgsdg, ',
+        address: ADDRESS_LIST[1],
+        type: 'Salle de musique',
+        price: 203, // par jour
+        size: 150, // en metre carré
+        capacity: 200,
+        roomCount: 2, // nombre de chambre
+        creationDate: new Date(),
+        uid: 'sv54sd6g4sd6g' // utilisateur id
+    },
+
+    {
+        _id: 'sdsdg54654sdg',
+        nameName: 'Le Void',
+        description: 'Dansgsdg, Dans un aire plein de musie oriontal sdgsgs sdgsdg, Dans un aire plein de musie oriontal sdgsgs sdgsdg, ',
+        address: ADDRESS_LIST[2],
+        type: 'Salle de musique',
+        price: 203, // par jour
+        size: 150, // en metre carré
+        capacity: 200,
+        roomCount: 2, // nombre de chambre
+        creationDate: new Date(),
+        uid: 'sv54sd6g4sd6g' // utilisateur id
+    },
+
+    {
+        _id: 'sdsdg54654sdg',
+        nameName: 'Le Void',
+        description: 'Dansgsdg, Dans un aire plein de musie oriontal sdgsgs sdgsdg, Dans un aire plein de musie oriontal sdgsgs sdgsdg, ',
+        address: ADDRESS_LIST[0],
+        type: 'Salle de musique',
+        price: 203, // par jour
+        size: 150, // en metre carré
+        capacity: 200,
+        roomCount: 2, // nombre de chambre
+        creationDate: new Date(),
+        uid: 'sv54sd6g4sd6g' // utilisateur id
+    },
+
+    {
+        _id: 'sdsdg54654sdg',
+        nameName: 'Le Void',
+        description: 'Dansgsdg, Dans un aire plein de musie oriontal sdgsgs sdgsdg, Dans un aire plein de musie oriontal sdgsgs sdgsdg, ',
+        address: ADDRESS_LIST[1],
+        type: 'Salle de musique',
+        price: 203, // par jour
+        size: 150, // en metre carré
+        capacity: 200,
+        roomCount: 2, // nombre de chambre
+        creationDate: new Date(),
+        uid: 'sv54sd6g4sd6g' // utilisateur id
+    },
+
+    {
+        _id: 'sdsdg54654sdg',
+        nameName: 'Le Void',
+        description: 'Dansgsdg, Dans un aire plein de musie oriontal sdgsgs sdgsdg, Dans un aire plein de musie oriontal sdgsgs sdgsdg, ',
+        address: ADDRESS_LIST[0],
+        type: 'Salle de musique',
+        price: 203, // par jour
+        size: 150, // en metre carré
+        capacity: 200,
+        roomCount: 2, // nombre de chambre
+        creationDate: new Date(),
+        uid: 'sv54sd6g4sd6g' // utilisateur id
+    },
+
+    {
+        _id: 'sdsdg54654sdg',
+        nameName: 'Le Void',
+        description: 'Dansgsdg, Dans un aire plein de musie oriontal sdgsgs sdgsdg, Dans un aire plein de musie oriontal sdgsgs sdgsdg, ',
+        address: ADDRESS_LIST[0],
+        type: 'Salle de musique',
+        price: 203, // par jour
+        size: 150, // en metre carré
+        capacity: 200,
+        roomCount: 2, // nombre de chambre
+        creationDate: new Date(),
+        uid: 'sv54sd6g4sd6g' // utilisateur id
+    }
+];
+
+export const ROOMS_LIST = roomList;
