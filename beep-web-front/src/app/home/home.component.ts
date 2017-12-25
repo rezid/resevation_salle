2ed7c8ed8a1d5ef79eb4c9162d7ef7f83b7f2cb1
zidane <zidane.rezzak@gmail.com>
2017-12-25 02:12:01 -0800

    add background image

--- beep-web-front/src/app/home/home.component.ts
+++ beep-web-front/src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Room } from '../core/models/room/room';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
